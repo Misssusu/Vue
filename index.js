f2d9332b40c74f15b2aeb74b82bc1cd3a6206c9d
suyuan <suyuan326@qq.com>
2019-07-13 20:15:52 +0800

    组件化应用构建

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
+
+Vue.component('todo-item',{
+    props: ['fruit'],
+    template: '<li>{{fruit}}</li>'
+})
 let app = new Vue({
     el: ".app",
     data: {
-        msg: 'hello',
-    },
-    methods: {
-        reverseMessage: function(){
-            console.dir(this)   //Vue
-            this.msg = this.msg.split('').reverse().join()
-        }
-    }
-})
-let app1= new Vue({
-    el: ".app1",
-    data: {
-        msg: 'hello v-model'
+        fruits: ['apple','orange','banana']
     }
 })
