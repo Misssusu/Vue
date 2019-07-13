633a17ccfb8dcc8a24942b20a51e09c82d8e442b
suyuan <suyuan326@qq.com>
2019-07-14 01:06:26 +0800

    模板语法

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-let obj = {
-    a: 1
-}
 let app = new Vue({
     el: ".app",
-    data: obj,
+    data: {
+        msg: 'hello'
+    }
+});
+let app2 = new Vue({
+    el: ".app2",
+    data: {
+        rawHtml: '<span style="color:red">this is red</span>'
+    }
+});
+let app3 = new Vue({
+    el: ".app3",
+    data: {
+        btnDisable: true,
+        dynamicId: 'div'
+    }
+});
+let app4 = new Vue({
+    el: ".app4",
+    data: {
+        number: 1,
+        id: 'div'
+    }
+});
+let app5 = new Vue({
+    el: ".app5",
+    data: {
+        eventname: 'click',
+        attribute: 'class',
+        str: 'item'
+    },
     methods: {
-        reduce: function(step){
-            this.a -= 1;
-            console.log(step);
+        doSomething: function(){
+            console.log('click')
         }
     }
 });
+
+
+
+
 
