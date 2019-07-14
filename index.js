27774039050c905382de1735e0c37fb36c77cca6
suyuan <suyuan326@qq.com>
2019-07-14 18:01:11 +0800

    计算属性和侦听器

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 let app = new Vue({
     el: ".app",
     data: {
-        msg: 'hello'
-    }
-});
-let app2 = new Vue({
-    el: ".app2",
-    data: {
-        rawHtml: '<span style="color:red">this is red</span>'
-    }
-});
-let app3 = new Vue({
-    el: ".app3",
-    data: {
-        btnDisable: true,
-        dynamicId: 'div'
-    }
-});
-let app4 = new Vue({
-    el: ".app4",
-    data: {
-        number: 1,
-        id: 'div'
-    }
-});
-let app5 = new Vue({
-    el: ".app5",
-    data: {
-        eventname: 'click',
-        attribute: 'class',
-        str: 'item'
+        message: 'hello'
+    },
+    computed: {
+        reverseMessage: function(){     //声明了一个计算属性reverseMessage
+            return this.message.split('').reverse().join('')
+        }
     },
     methods: {
-        doSomething: function(){
-            console.log('click')
+        reverseMessageFn: function(){
+            return this.message.split('').reverse().join('')
         }
     }
 });
+let app1 = new Vue({
+    el: ".app1",
+    data: {
+        firstName: 'Su',
+        lastName: 'Yuan',
+        fullNameWatch: 'Su Yuan'
+    },
+    watch: {
+        firstName: function(val) {
+            this.fullNameWatch = val + ' ' + this.lastName
+        },
+        lastName: function(val) {
+            this.fullNameWatch = this.firstName + ' ' + val
+        }
+    },
+    computed: {
+        // fullName() {
+        //     return this.firstName + ' ' + this.lastName
+        // },
+        fullName: {
+            get: function () {
+                return this.firstName + ' ' + this.lastName
+            },
+            set: function (newVall) {
+                let names = newVall.split(' ');
+                this.firstName = names[0];
+                this.lastName = names[names.length-1];
+            }
+        }
+    }
+});
+
+
 
 
 
