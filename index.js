3561fb3a828bc6b2b2d03bcf52781be082ce8ae6
suyuan <suyuan326@qq.com>
2019-07-14 23:39:18 +0800

    Class 与 Style 绑定

--- index.js
+++ index.js
@@ -1,50 +1,31 @@
+Vue.component('my-component',{
+    template: '<p class="foo bar">用在组件上</p>'
+});
 let app = new Vue({
     el: ".app",
     data: {
-        message: 'hello'
-    },
-    computed: {
-        reverseMessage: function(){     //声明了一个计算属性reverseMessage
-            return this.message.split('').reverse().join('')
-        }
-    },
-    methods: {
-        reverseMessageFn: function(){
-            return this.message.split('').reverse().join('')
-        }
+        classObj: {
+            active: true,
+            red: false
+        },
+        activeClass: 'isActive',
+        errorClass: 'error'
     }
 });
 let app1 = new Vue({
     el: ".app1",
     data: {
-        firstName: 'Su',
-        lastName: 'Yuan',
-        fullNameWatch: 'Su Yuan'
-    },
-    watch: {
-        firstName: function(val) {
-            this.fullNameWatch = val + ' ' + this.lastName
+        styleObj: {
+            color: 'blue'
         },
-        lastName: function(val) {
-            this.fullNameWatch = this.firstName + ' ' + val
-        }
-    },
-    computed: {
-        // fullName() {
-        //     return this.firstName + ' ' + this.lastName
-        // },
-        fullName: {
-            get: function () {
-                return this.firstName + ' ' + this.lastName
-            },
-            set: function (newVall) {
-                let names = newVall.split(' ');
-                this.firstName = names[0];
-                this.lastName = names[names.length-1];
-            }
+        newStyle: {
+            fontWeight: 'bold'
         }
     }
 });
+let app2 = new Vue({
+    el: ".app2"
+})
 
 
 
