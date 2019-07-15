05ecf3c918315eb64f6635f065a24215e66e686a
suyuan <suyuan326@qq.com>
2019-07-15 16:26:05 +0800

    条件渲染

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-Vue.component('my-component',{
-    template: '<p class="foo bar">用在组件上</p>'
-});
 let app = new Vue({
     el: ".app",
     data: {
-        classObj: {
-            active: true,
-            red: false
-        },
-        activeClass: 'isActive',
-        errorClass: 'error'
+        isShow: true,
+        show: true,
+        type: 'A',
+        loginType: 'username',
+        vshow: true
     }
 });
-let app1 = new Vue({
-    el: ".app1",
-    data: {
-        styleObj: {
-            color: 'blue'
-        },
-        newStyle: {
-            fontWeight: 'bold'
-        }
-    }
-});
-let app2 = new Vue({
-    el: ".app2"
-})
-
 
 
 
