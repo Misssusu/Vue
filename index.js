cbc092f6015c8d380658c0344cfdae3028202d16
suyuan <suyuan326@qq.com>
2019-07-16 15:05:26 +0800

    事件处理

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ let app = new Vue({
             name: 'suyuan',
             age: '18'
         }
+    },
+    methods: {
+        submit: function(evt){
+            console.log(evt)
+        },
+        doThis: function(){
+            console.log('child click')
+        },
+        isClick() {
+            console.log('parent click')
+        },
+        enterSubmit() {
+            console.log('press enter')
+        },
+        backSpaceFn() {
+            console.log('press backspace');
+        },
+        onCtrlPress() {
+            console.log('press ctrl');
+        },
+        leftPress() {
+            console.log('click left');
+        }
     }
 });
 
